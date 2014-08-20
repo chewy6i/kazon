d1da3ff2fdbe57f7b3969302499a26dfaa9e0271
salla200 <sridhar_alla@cable.comcast.com>
2014-08-19 21:23:32 -0400

    added empty bolt, spout and topology

--- src/main/java/org/apache/kazon/storm/KazonSpout.java
+++ src/main/java/org/apache/kazon/storm/KazonSpout.java
@@ -1,5 +1,32 @@
 package org.apache.kazon.storm;
 
-public class KazonSpout {
+import java.util.Map;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+
+public class KazonSpout extends BaseRichSpout {
+
+	private static final long serialVersionUID = 7635156906752519841L;
+
+	@Override
+	public void nextTuple() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void open(Map arg0, TopologyContext arg1, SpoutOutputCollector arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/main/java/org/apache/kazon/storm/KazonTopology.java
+++ src/main/java/org/apache/kazon/storm/KazonTopology.java
@@ -1,5 +1,13 @@
 package org.apache.kazon.storm;
 
+import backtype.storm.generated.StormTopology;
+import backtype.storm.topology.TopologyBuilder;
+
 public class KazonTopology {
 
+	public StormTopology createTopology() {
+		TopologyBuilder topologyBuilder = new TopologyBuilder();
+		
+		return topologyBuilder.createTopology();
+	}
 }
